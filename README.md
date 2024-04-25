24a4160eb6abdca4c0333a12d24dd91f373fcc45
foye3 <zffoye@gmail.com>
2024-04-24 22:13:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# visited-states-us
+A basic web page that will let you mark which cities you have visited in China.
+
+https://foye3.github.io/visited-cities-china/
---
+++ script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var table;
+var language;
+
+function popTable(e) {
+    table.city = e.currentTarget.city;
+    table.getElementsByTagName('th')[0].textContent = table.city.name;
+    table.style.display = 'table';
+    table.style.left = e.clientX + 'px';
+    table.style.top = e.clientY + 'px';
+    e.stopPropagation();
+}
+
+function showName(e) {
+    e.node.attr({opacity: 1});
+}
+
+function hideName(e) {
+    e.node.attr({opacity: 0});
+}
+
+function handleSelect(e) {
+    var select = +e.currentTarget.getAttribute('data-level');
+    table.city.level = select;
+    table.city.graph.attr({fill : color[select]});
+    document.getElementById('level').textContent = calculate();
+    table.style.display = 'none';
+}
+
+var color = ['#d3d3d3','#3598db', '#30cc70', '#f3c218', '#d58337', '#e84c3d'];
+
+function calculate() {
+    var sum = 0;
+    for (var city in chinaMap) {
+        sum += chinaMap[city].level;
+    }
+    return sum;
+}
+
+window.onload = function () {
+    table = document.getElementById('table');
+    var map = new Raphael('map',2000, 4000);
+    var attr = {
+            "fill": "#d3d3d3",
+            "stroke": "#fff",
+            "stroke-opacity": "1",
+            "stroke-linejoin": "round",
+            "stroke-miterlimit": "4",
+            "stroke-width": "0.75",
+            "stroke-dasharray": "none"
+        };
+
+
+    for (var key in chinaMap) {
+        var city = chinaMap[key]
+        var path = city.path;
+        var graph = map.path(path).scale(1.8, 2, 0, 2);
+        graph.attr(attr);
+        city.graph = graph;
+        city.level = 0;
+        var x = graph.getBBox().x + graph.getBBox().width / 2 + city.offset.x;
+        var y = graph.getBBox().y + graph.getBBox().height / 2 + city.offset.y;
+        // var text = map.text(x, y, key);
+
+        [graph].forEach(
+            function (e) {
+                e.attr({cursor : 'pointer'});
+                e[0].onclick = popTable;
+                e[0].city = city;
+                // e.node.onmouseover = showName;
+                // e.node.onmouseout = hideName;
+            }
+        );
+    }
+
+    Array.from(document.getElementsByClassName('select')).forEach(
+        function (select) {
+            select.style.backgroundColor = color[select.getAttribute('data-level')];
+            select.onclick = handleSelect;
+        }
+    );
+
+    document.getElementsByTagName('html')[0].onclick = function () {
+        table.style.display = 'none';
+    };
+};
+
